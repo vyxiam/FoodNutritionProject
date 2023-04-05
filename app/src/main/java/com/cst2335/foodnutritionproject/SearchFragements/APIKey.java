3b12d13829c0841918c8c51a5772dd6a55ef5625
Vy Nguyen <vyx.nguyen96@gmail.com>
2023-04-05 14:06:39 -0400

    Updated Search

--- app/src/main/java/com/cst2335/foodnutritionproject/SearchFragements/APIKey.java
+++ app/src/main/java/com/cst2335/foodnutritionproject/SearchFragements/APIKey.java
@@ -1,6 +1,18 @@
 package com.cst2335.foodnutritionproject.SearchFragements;
 
+import com.cst2335.foodnutritionproject.Utility.CustomViewUtility;
+
+/**
+ * This enum contains the Credentials of API request including ID, KEY and the URL to access
+ * the Food API
+ *
+ * @author Van Vy Nguyen
+ * @version 1.0
+ */
 enum APIKey {
+    /**
+     * Enum value represents the Request API URL
+     */
     URL(APIKey.REQUEST_URL);
     private static final String APPLICATION_ID = "dd756cb3";
     private static final String APPLICATION_KEY = "5940ed464424df230e4e99d060a36e00";
@@ -17,6 +29,11 @@ enum APIKey {
     APIKey(String value) {
         this.value = value;
     }
+
+    /**
+     * This setter method is used to get the URL link included ID, and KEY to access the API
+     * @return the link to access the API
+     */
     public String getValue(){
         return value;
     }
--- app/src/main/java/com/cst2335/foodnutritionproject/SearchFragements/SearchDetailsFragment.java
+++ app/src/main/java/com/cst2335/foodnutritionproject/SearchFragements/SearchDetailsFragment.java
@@ -101,6 +101,9 @@ public class SearchDetailsFragment extends Fragment {
 
     }
 
+    /**
+     * This helper private method is used to initialize the item attributes.
+     */
     private void initialize(){
         if (foodList == null)
             foodList = new ArrayList<>();
@@ -112,6 +115,9 @@ public class SearchDetailsFragment extends Fragment {
         fragmentSearchDetailsBinding.detailsFavoriteButton.setOnClickListener(view -> showDialog());
     }
 
+    /**
+     * This helper method is used to show an instruction for the fragment.
+     */
     private void showDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         builder.setTitle(getString(R.string.details_favorite_instruction_title))
@@ -125,6 +131,10 @@ public class SearchDetailsFragment extends Fragment {
 
     }
 
+    /**
+     * This private helper method is used to add the food to the database. This method also calls other
+     * helper methods to check whether the food is stored in the database or not.
+     */
     private void favoriteFood(){
         boolean isExistInDatabase = checkDatabaseContent(food.getFoodID());
         if (isExistInDatabase){
@@ -136,6 +146,11 @@ public class SearchDetailsFragment extends Fragment {
         }
     }
 
+    /**
+     * This method is used to help refresh the items in the fragment with new information by the passed
+     * in Bundle object. It contains the Food name, description and nutrients of the food.
+     * @param bundle the Bundle object stores new information
+     */
     public void loadDetails(Bundle bundle){
         if (food == null)
             food = new Food();
@@ -164,6 +179,13 @@ public class SearchDetailsFragment extends Fragment {
                 String.format("%.2f",food.getCarbohydrate()));
     }
 
+    /**
+     * This private helper method is used to check the passed in foodID whether it is stored in the database
+     * or not and return a boolean value based on this situation. This is used to help determine whether user
+     * wants to add an exist food to the database to avoid duplicated content.
+     * @param foodID the passed-in food ID to check
+     * @return true if the foodID is stored, false if the foodID doesn't exist in the database
+     */
     private boolean checkDatabaseContent(String foodID){
         database = FoodDatabase.getInstance(requireContext());
         foodDAO = database.foodDAO();
@@ -178,6 +200,11 @@ public class SearchDetailsFragment extends Fragment {
         return result;
     }
 
+    /**
+     * This private helper method is used to get all the Food data from the database. This will
+     * return a List<Food> and added into the foodList instance variable. It also uses CountDownLatch
+     * to lock the UI thread to ensure all Foods are loaded before the logic is executed.
+     */
     private void getAllFoodInDatabase(){
         new Thread(() -> {
             foodList.addAll(foodDAO.getAllFood());
@@ -185,6 +212,11 @@ public class SearchDetailsFragment extends Fragment {
         }).start();
     }
 
+    /**
+     * This private helper method is used to access DAO and add passed-in Food object into the database
+     * using a separate thread.
+     * @param food the Food object will be added into database
+     */
     private void favoriteFood(Food food){
         new Thread(() -> {
             foodDAO.addFood(food);
